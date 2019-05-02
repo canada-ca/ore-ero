9b7263cd6b9133e3b93541da5365a1376545ec55
j-rewerts <jaredrewerts@gmail.com>
2019-05-02 10:31:32 -0600

    Prettified code.

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -3,118 +3,125 @@
 /* global $ YamlWriter USERNAME REPO_NAME YAML PRBOT_URL */
 
 function getSelectedOrgType() {
-  return $("#adminCode :selected").parent().attr('label').toLowerCase();
+  return $('#adminCode :selected')
+    .parent()
+    .attr('label')
+    .toLowerCase();
 }
 
-$("#prbotSubmit").click(function () {
+$('#prbotSubmit').click(function() {
   let content =
-    "" +
+    '' +
     `releases:
   -
     contact: 
       URL: 
-        en: ${$("#enUrlContact").val()}
-        fr: ${$("#frUrlContact").val()}
+        en: ${$('#enUrlContact').val()}
+        fr: ${$('#frUrlContact').val()}
     date: 
-      created: ${$("#dateCreated").val()}
-      metadataLastUpdated: ${$("#dateLastUpdated").val()}
+      created: ${$('#dateCreated').val()}
+      metadataLastUpdated: ${$('#dateLastUpdated').val()}
     description: 
-      en: ${$("#enDescription").val()}
-      fr: ${$("#frDescription").val()}
+      en: ${$('#enDescription').val()}
+      fr: ${$('#frDescription').val()}
     name: 
-      en: ${$("#enProjectName").val()}
-      fr: ${$("#frProjectName").val()}
+      en: ${$('#enProjectName').val()}
+      fr: ${$('#frProjectName').val()}
     licenses: 
       - 
         URL: 
-          en: ${$("#enUrlLicense").val()}
-          fr: ${$("#frUrlLicense").val()}
-          spdxID: ${$("#spdxID").val()}
+          en: ${$('#enUrlLicense').val()}
+          fr: ${$('#frUrlLicense').val()}
+          spdxID: ${$('#spdxID').val()}
     repositoryURL: 
-      en: ${$("#enRepoUrl").val()}
-      fr: ${$("#frRepoUrl").val()}
-    status: ${$("#status :selected").text()}
+      en: ${$('#enRepoUrl').val()}
+      fr: ${$('#frRepoUrl').val()}
+    status: ${$('#status :selected').text()}
     tags: 
       en: 
-${[...document.querySelectorAll("#tagsEN input")]
-      .map(child => child.value)
-      .map(tag => '        - "' + tag + '"')
-      .join("\n")}
+${[...document.querySelectorAll('#tagsEN input')]
+  .map(child => child.value)
+  .map(tag => '        - "' + tag + '"')
+  .join('\n')}
       fr: 
-${[...document.querySelectorAll("#tagsFR input")]
-      .map(child => child.value)
-      .map(tag => '        - "' + tag + '"')
-      .join("\n")}
-    vcs: ${$("#vcs").val()}
+${[...document.querySelectorAll('#tagsFR input')]
+  .map(child => child.value)
+  .map(tag => '        - "' + tag + '"')
+  .join('\n')}
+    vcs: ${$('#vcs').val()}
 `;
   let fileWriter = new YamlWriter(USERNAME, REPO_NAME);
-  let file = `_data/code/${getSelectedOrgType()}/${$("#adminCode").val()}.yml`;
-  fileWriter.merge(file, content, "releases", "name.en").then(result => {
-    const config = {
-      body: JSON.stringify({
-        user: USERNAME,
-        repo: REPO_NAME,
-        title: "Updated code for " + $("#adminCode :selected").text(),
-        description:
-          "Authored by: " +
-          $("#emailContact").val() +
-          "\n" +
-          "Project: ***" +
-          $("#enProjectName").val() +
-          "***\n" +
-          $("#enDescription").val() +
-          "\n",
-        commit: "Committed by " + $("#emailContact").val(),
-        author: {
-          name: $("#nameContact").val(),
-          email: $("#emailContact").val()
-        },
-        files: [
-          {
-            path: file,
-            content: YAML.stringify(result, { keepBlobsInJSON: false })
-          }
-        ]
-      }),
-      method: "POST"
-    };
-    return fetch(PRBOT_URL, config);
-  }).catch(err => {
-    if (err.status == 404) {
-      // We need to create the file for this organization, as it doesn't yet exist.
-      let header = `schemaVersion: "1.0"\nadminCode: ${$("#adminCode").val()}\n`
+  let file = `_data/code/${getSelectedOrgType()}/${$('#adminCode').val()}.yml`;
+  fileWriter
+    .merge(file, content, 'releases', 'name.en')
+    .then(result => {
       const config = {
         body: JSON.stringify({
           user: USERNAME,
           repo: REPO_NAME,
-          title: "Created code file for " + $("#adminCode :selected").text(),
+          title: 'Updated code for ' + $('#adminCode :selected').text(),
           description:
-            "Authored by: " +
-            $("#emailContact").val() +
-            "\n" +
-            "Project: ***" +
-            $("#enProjectName").val() +
-            "***\n" +
-            $("#enDescription").val() +
-            "\n",
-          commit: "Committed by " + $("#emailContact").val(),
+            'Authored by: ' +
+            $('#emailContact').val() +
+            '\n' +
+            'Project: ***' +
+            $('#enProjectName').val() +
+            '***\n' +
+            $('#enDescription').val() +
+            '\n',
+          commit: 'Committed by ' + $('#emailContact').val(),
           author: {
-            name: $("#nameContact").val(),
-            email: $("#emailContact").val()
+            name: $('#nameContact').val(),
+            email: $('#emailContact').val()
           },
           files: [
             {
               path: file,
-              content: header + content
+              content: YAML.stringify(result, { keepBlobsInJSON: false })
             }
           ]
         }),
-        method: "POST"
+        method: 'POST'
       };
       return fetch(PRBOT_URL, config);
-    }
-    else {
-      throw err;
-    }
-  });
+    })
+    .catch(err => {
+      if (err.status == 404) {
+        // We need to create the file for this organization, as it doesn't yet exist.
+        let header = `schemaVersion: "1.0"\nadminCode: ${$(
+          '#adminCode'
+        ).val()}\n`;
+        const config = {
+          body: JSON.stringify({
+            user: USERNAME,
+            repo: REPO_NAME,
+            title: 'Created code file for ' + $('#adminCode :selected').text(),
+            description:
+              'Authored by: ' +
+              $('#emailContact').val() +
+              '\n' +
+              'Project: ***' +
+              $('#enProjectName').val() +
+              '***\n' +
+              $('#enDescription').val() +
+              '\n',
+            commit: 'Committed by ' + $('#emailContact').val(),
+            author: {
+              name: $('#nameContact').val(),
+              email: $('#emailContact').val()
+            },
+            files: [
+              {
+                path: file,
+                content: header + content
+              }
+            ]
+          }),
+          method: 'POST'
+        };
+        return fetch(PRBOT_URL, config);
+      } else {
+        throw err;
+      }
+    });
 });
