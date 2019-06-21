9c1a767a9fd6e92f791672ad30222d4e2d8190aa
BassamAjam <bassam.ajam95@gmail.com>
2019-06-21 17:28:13 -0400

    prettify code

--- assets/js/src/codeForm.js
+++ assets/js/src/codeForm.js
@@ -10,17 +10,17 @@
 const codeObj = $('.page-codeForm #ProjectNameSelect');
 const adminObj = $('.page-codeForm #adminCode');
 
-function addNewAdmin() {
+$('#newAmdminButton').click(function() {
   $('#newAdmin').removeClass('hide');
-  $('#adminCode').removeAttr('required');  
-  $('label[for="adminCode"]').removeClass("required");
-}
+  $('#adminCode').removeAttr('required');
+  $('label[for="adminCode"]').removeClass('required');
+});
 
-function removeNewAdmin() {
+$('#removeNewAdminButton').click(function() {
   $('#newAdmin').addClass('hide');
-  $('#adminCode').attr('required','required');  
-  $('label[for="adminCode"]').addClass("required");
-}
+  $('#adminCode').attr('required', 'required');
+  $('label[for="adminCode"]').addClass('required');
+});
 
 $(document).ready(function() {
   adminObj.focus();
