11df3c9eced36f78dc8ed5c70196c44cc2602b4f
Imade Zarrari <46538305+imadeDev@users.noreply.github.com>
2019-08-14 18:07:13 -0400

    Fix lint errors

--- assets/js/src/slugify.js
+++ assets/js/src/slugify.js
@@ -6,14 +6,18 @@ function slugify(string) {
     'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------';
   const p = new RegExp(a.split('').join('|'), 'g');
 
-  return string
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, ''); // Trim - from end of text
+  return (
+    string
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      // eslint-disable-next-line no-useless-escape
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      // eslint-disable-next-line no-useless-escape
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '')
+  ); // Trim - from end of text
 }
