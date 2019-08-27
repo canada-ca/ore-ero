33540b20fc4ed3b171f25e9549476abc7986adfc
Frédéric Lestage <frederic.lestage@ville.montreal.qc.ca>
2019-08-27 16:55:16 -0400

    Added missing pieces & base JS

--- assets/js/src/codeForm.js
+++ assets/js/src/codeForm.js
@@ -4,8 +4,9 @@
   USERNAME REPO_NAME PRBOT_URL
   getTagsEN getTagsFR resetTags addTags getLanguages selectLanguage resetLanguages
   submitInit submitConclusion
-  getAdminObject getAdminCode slugify
+  getAdminObject getAdminCode getSelectedOrgType getOrgLevel slugify
   addMoreLicences addMoreRelatedCode resetMoreGroup addMoreGroup fillLicenceField
+  addMorePartners getNewAdminPartnerPromise fillPartnersField
   getToday
 */
 
@@ -26,50 +27,6 @@ $(document).ready(function() {
 
   codeSelect.change(function() {
     selectCode();
-  });
-
-  // More-group overwrite for partner
-  $('.add-more-group#addMorepartners').on(
-    'click',
-    '.btn-tabs-more',
-    function() {
-      let length = $('#addMorepartners ul li').length;
-      let index = length == 1 ? '' : length - 1;
-      hideFieldsPartner(index);
-    }
-  );
-
-  hideFieldsPartner('');
-  $('#addMorepartners').on(
-    'change',
-    '.partnersAdminCodeSelect select',
-    function() {
-      selectPartners(this);
-    }
-  );
-  $('#addMorepartners').on('click', '.partnersAdminCodeBtn button', function() {
-    addNewPartner(this);
-  });
-  $('#addMorepartners').on(
-    'click',
-    '.partnersAdminCodeRemove button',
-    function() {
-      removeNewPartner(this);
-    }
-  );
-  $('#addMorepartners').on('change', '.orgLevelPartner select', function() {
-    let index = getmoreIndex($(this));
-    if ($(this).val() == 'municipal') {
-      $('#provinceSelectPartner' + index)
-        .attr('required', 'required')
-        .siblings('label')
-        .addClass('required');
-    } else {
-      $('#provinceSelectPartner' + index)
-        .removeAttr('required')
-        .siblings('label')
-        .removeClass('required');
-    }
   });
 
   $('#formReset').click(function() {
@@ -168,42 +125,7 @@ function getCodeObject() {
   }
 
   // Optional more-group
-  $('#addMorepartners ul.list-unstyled > li').each(function(i) {
-    let id = i == 0 ? '' : i;
-
-    let adminCode = '';
-    if ($('#partners' + id).val() != '') adminCode = $('#partners' + id).val();
-    else if (
-      $('#enpartnersname' + id).val() != '' &&
-      $('#provinceSelectPartner' + id).val() != ''
-    ) {
-      adminCode = slugify(
-        $('#enpartnersname' + id).val() +
-          '-' +
-          $('#provinceSelectPartner' + id).val()
-      );
-    }
-
-    if (
-      $('#partnerscontactemail' + id).val() ||
-      $('#partnerscontactname' + id).val() ||
-      adminCode != ''
-    ) {
-      if (codeObject.releases[0].partners == undefined)
-        codeObject.releases[0].partners = [];
-      codeObject.releases[0].partners[i] = {};
-    }
-
-    codeObject.releases[0].partners[i].adminCode = adminCode;
-
-    codeObject.releases[0].partners[i].email = $(
-      '#partnerscontactemail' + id
-    ).val();
-
-    codeObject.releases[0].partners[i].name = $(
-      '#partnerscontactname' + id
-    ).val();
-  });
+  addMorePartners(codeObject.releases[0]);
 
   addMoreRelatedCode(codeObject.releases[0]);
 
@@ -222,15 +144,6 @@ function getCodeObject() {
   }
 
   return codeObject;
-}
-
-function getSelectedOrgType() {
-  if ($('#adminCode').val() != '')
-    return $('#adminCode :selected')
-      .parent()
-      .attr('label')
-      .toLowerCase();
-  else return $('#orgLevel').val();
 }
 
 function submitFormAdminCodeForm() {
@@ -292,67 +205,6 @@ function submitFormAdminCodeForm() {
             });
         });
     });
-}
-
-function getNewAdminPartnerPromise(codeObject, fileWriter, config) {
-  let promises = [];
-
-  if (
-    codeObject.releases[0].partners &&
-    codeObject.releases[0].partners.length > 0
-  ) {
-    let newAdmins = [];
-    codeObject.releases[0].partners.forEach(function(partner, index) {
-      let id = index == 0 ? '' : index;
-      if ($('#partners' + id).val() == '') {
-        if (newAdmins[$('#orgLevelPartner' + id).val()] == null)
-          newAdmins[$('#orgLevelPartner' + id).val()] = [];
-        newAdmins[$('#orgLevelPartner' + id).val()][
-          Object.keys(newAdmins[$('#orgLevelPartner' + id).val()]).length
-        ] = getNewAdminPartnerObject(id);
-      }
-    });
-
-    Object.keys(newAdmins).forEach(function(orgLevel) {
-      let promise = fileWriter
-        .mergePartnerAdminFile(
-          `_data/administrations/${orgLevel}.yml`,
-          newAdmins[orgLevel],
-          '',
-          'code'
-        )
-        .then(result => {
-          config.body.files[config.body.files.length] = {
-            path: `_data/administrations/${orgLevel}.yml`,
-            content: '---\n' + jsyaml.dump(result)
-          };
-        });
-
-      promises.push(promise);
-    });
-
-    return promises;
-  }
-}
-
-function getNewAdminPartnerObject(index) {
-  let adminObj = {
-    code: slugify(
-      $('#enpartnersname' + index).val() +
-        '-' +
-        $('#provinceSelectPartner' + index).val()
-    ),
-    name: {
-      en: $('#enpartnersname' + index).val(),
-      fr: $('#frpartnersname' + index).val()
-    }
-  };
-
-  // Optional fields
-  let province = $('#provinceSelectPartner' + index).val();
-  if (province != '') adminObj.provinceCode = province;
-
-  return adminObj;
 }
 
 function getConfigNewAdmin(
@@ -576,20 +428,6 @@ function selectCode() {
   }
 }
 
-function getOrgLevel(result, admin) {
-  let federal = result.federal[admin];
-  let provincial = result.provincial[admin];
-  let municipal = result.municipal[admin];
-
-  let orgLevel;
-
-  if (municipal != undefined) orgLevel = municipal;
-  else if (provincial != undefined) orgLevel = provincial;
-  else if (federal != undefined) orgLevel = federal;
-
-  return orgLevel;
-}
-
 function addValueToFields(obj) {
   resetFields();
 
@@ -635,19 +473,7 @@ function addValueToFields(obj) {
     });
   }
 
-  if (obj.partners)
-    obj.partners.forEach(function(partner, i) {
-      let id;
-      if (i == 0) id = '';
-      else {
-        id = i;
-        addMoreGroup($('#addMorepartners'));
-      }
-      $('#partners' + id).val(partner.adminCode);
-      showFieldsPartner(id, false);
-      if (partner.email) $('#partnerscontactemail' + id).val(partner.email);
-      if (partner.name) $('#partnerscontactname' + id).val(partner.name);
-    });
+  fillPartnersField(obj);
 
   if (obj.relatedCode)
     obj.relatedCode.forEach(function(related, i) {
@@ -700,149 +526,4 @@ function resetFields() {
   resetMoreGroup($('#addMorepartners'));
   resetMoreGroup($('#addMorerelatedCode'));
   $('#status').val('');
-}
-
-function getAdminObjectForPartner(obj, admin) {
-  let administrations = [
-    'federal',
-    'provincial',
-    'municipal',
-    'aboriginal',
-    'others'
-  ];
-
-  for (let i = 0, l1 = administrations.length; i < l1; i++)
-    for (let j = 0, l2 = obj[administrations[i]].length; j < l2; j++)
-      if (obj[administrations[i]][j].code == admin)
-        return {
-          level: administrations[i],
-          values: obj[administrations[i]][j]
-        };
-}
-
-function getmoreIndex(element) {
-  let nb = $(element)
-    .closest('li')
-    .attr('data-index');
-  return nb != 0 ? nb : '';
-}
-
-function selectPartners(select) {
-  let adminCode = $(select).val();
-  let id = getmoreIndex(select);
-  if (adminCode != '') {
-    $.getJSON(
-      'https://canada-ca.github.io/ore-ero/administrations.json',
-      function(result) {
-        let admin = getAdminObjectForPartner(result, adminCode);
-        showFieldsPartner(id, false);
-        $('#orgLevelPartner' + id).val(admin.level);
-        if (admin.values.provinceCode != undefined) {
-          $('#provinceSelectPartner' + id)
-            .prop('disabled', false)
-            .val(admin.values.provinceCode);
-        } else
-          $('#provinceSelectPartner' + id)
-            .prop('disabled', true)
-            .val('');
-        $('#enpartnersname' + id).val(admin.values.name.en);
-        $('#frpartnersname' + id).val(admin.values.name.fr);
-      }
-    );
-  } else {
-    hideFieldsPartner(id);
-  }
-}
-
-function hideFieldsPartner(id) {
-  resetFieldsPartner(id);
-  hideFieldPartner($('#orgLevelPartner' + id));
-  hideFieldPartner($('#provinceSelectPartner' + id));
-  hideFieldPartner($('#enpartnersname' + id));
-  hideFieldPartner($('#frpartnersname' + id));
-  $('#partnersNewAdminSeparator' + id).hide();
-  hideFieldPartner($('#partnerscontactname' + id));
-  hideFieldPartner($('#partnerscontactemail' + id));
-}
-
-function hideFieldPartner(element) {
-  element
-    .prop('disabled', false)
-    .parent('.form-group')
-    .hide();
-}
-
-function resetFieldsPartner(id) {
-  $('#orgLevelPartner' + id).val('');
-  $('#provinceSelectPartner' + id)
-    .prop('disabled', false)
-    .val('');
-  $('#enpartnersname' + id).val('');
-  $('#frpartnersname' + id).val('');
-  $('#partnerscontactname' + id).val('');
-  $('#partnerscontactemail' + id).val('');
-}
-
-function showFieldsPartner(id, full) {
-  hideFieldsPartner(id);
-  if (full) {
-    showFieldPartner($('#orgLevelPartner' + id));
-    showFieldPartner($('#provinceSelectPartner' + id));
-    if ($('#orgLevelPartner' + id).val() != 'municipal')
-      $('#provinceSelectPartner' + id)
-        .removeAttr('required')
-        .siblings('label')
-        .removeClass('required');
-    showFieldPartner($('#enpartnersname' + id));
-    showFieldPartner($('#frpartnersname' + id));
-    $('#partnersNewAdminSeparator' + id).show();
-  }
-
-  showFieldPartnerOptional(id);
-}
-
-function showFieldPartner(element) {
-  element
-    .attr('required', 'required')
-    .prop('disabled', false)
-    .parent('.form-group')
-    .show()
-    .children('label')
-    .addClass('required');
-}
-
-function showFieldPartnerOptional(id) {
-  $('#partnerscontactname' + id)
-    .parent('.form-group')
-    .show();
-
-  $('#partnerscontactemail' + id)
-    .parent('.form-group')
-    .show();
-}
-
-function addNewPartner(button) {
-  let id = getmoreIndex(button);
-
-  $('#partners' + id).val('');
-  resetFieldsPartner(id);
-  showFieldsPartner(id, true);
-  showRemoveNewPartnerBtn(id);
-}
-
-function removeNewPartner(button) {
-  let id = getmoreIndex(button);
-
-  $('#partners' + id).val('');
-  resetFieldsPartner(id);
-  hideFieldsPartner(id);
-  hideRemoveNewPartnerBtn(id);
-}
-
-function showRemoveNewPartnerBtn(index) {
-  $('newAdminButtonpartners' + index).hide();
-}
-
-function hideRemoveNewPartnerBtn(index) {
-  $('newAdminButtonpartners' + index).hide();
 }
---
+++ assets/js/src/partners.js
@@ -0,0 +1,301 @@
+/* global $ jsyaml slugify addMoreGroup */
+/* exported fillPartnersField getNewAdminPartnerPromise addMorePartners */
+
+$(document).ready(function() {
+  // More-group overwrite for partner
+  $('.add-more-group#addMorepartners').on(
+    'click',
+    '.btn-tabs-more',
+    function() {
+      let length = $('#addMorepartners ul li').length;
+      let index = length == 1 ? '' : length - 1;
+      hideFieldsPartner(index);
+    }
+  );
+
+  hideFieldsPartner('');
+  $('#addMorepartners').on(
+    'change',
+    '.partnersAdminCodeSelect select',
+    function() {
+      selectPartners(this);
+    }
+  );
+  $('#addMorepartners').on('click', '.partnersAdminCodeBtn button', function() {
+    addNewPartner(this);
+  });
+  $('#addMorepartners').on(
+    'click',
+    '.partnersAdminCodeRemove button',
+    function() {
+      removeNewPartner(this);
+    }
+  );
+  $('#addMorepartners').on('change', '.orgLevelPartner select', function() {
+    let index = getmoreIndex($(this));
+    if ($(this).val() == 'municipal') {
+      $('#provinceSelectPartner' + index)
+        .attr('required', 'required')
+        .siblings('label')
+        .addClass('required');
+    } else {
+      $('#provinceSelectPartner' + index)
+        .removeAttr('required')
+        .siblings('label')
+        .removeClass('required');
+    }
+  });
+});
+
+function addMorePartners(obj) {
+  $('#addMorepartners ul.list-unstyled > li').each(function(i) {
+    let id = i == 0 ? '' : i;
+
+    let adminCode = '';
+    if ($('#partners' + id).val() != '') adminCode = $('#partners' + id).val();
+    else if (
+      $('#enpartnersname' + id).val() != '' &&
+      $('#provinceSelectPartner' + id).val() != ''
+    ) {
+      adminCode = slugify(
+        $('#enpartnersname' + id).val() +
+          '-' +
+          $('#provinceSelectPartner' + id).val()
+      );
+    }
+
+    if (
+      $('#partnerscontactemail' + id).val() ||
+      $('#partnerscontactname' + id).val() ||
+      adminCode != ''
+    ) {
+      if (obj.partners == undefined) obj.partners = [];
+      obj.partners[i] = {};
+    }
+
+    obj.partners[i].adminCode = adminCode;
+
+    obj.partners[i].email = $('#partnerscontactemail' + id).val();
+
+    obj.partners[i].name = $('#partnerscontactname' + id).val();
+  });
+}
+
+function getNewAdminPartnerPromise(obj, fileWriter, config) {
+  let promises = [];
+
+  if (obj.partners && obj.partners.length > 0) {
+    let newAdmins = [];
+    obj.partners.forEach(function(partner, index) {
+      let id = index == 0 ? '' : index;
+      if ($('#partners' + id).val() == '') {
+        if (newAdmins[$('#orgLevelPartner' + id).val()] == null)
+          newAdmins[$('#orgLevelPartner' + id).val()] = [];
+        newAdmins[$('#orgLevelPartner' + id).val()][
+          Object.keys(newAdmins[$('#orgLevelPartner' + id).val()]).length
+        ] = getNewAdminPartnerObject(id);
+      }
+    });
+
+    Object.keys(newAdmins).forEach(function(orgLevel) {
+      let promise = fileWriter
+        .mergePartnerAdminFile(
+          `_data/administrations/${orgLevel}.yml`,
+          newAdmins[orgLevel],
+          '',
+          'code'
+        )
+        .then(result => {
+          config.body.files[config.body.files.length] = {
+            path: `_data/administrations/${orgLevel}.yml`,
+            content: '---\n' + jsyaml.dump(result)
+          };
+        });
+
+      promises.push(promise);
+    });
+
+    return promises;
+  }
+}
+
+function getNewAdminPartnerObject(index) {
+  let adminObj = {
+    code: slugify(
+      $('#enpartnersname' + index).val() +
+        '-' +
+        $('#provinceSelectPartner' + index).val()
+    ),
+    name: {
+      en: $('#enpartnersname' + index).val(),
+      fr: $('#frpartnersname' + index).val()
+    }
+  };
+
+  // Optional fields
+  let province = $('#provinceSelectPartner' + index).val();
+  if (province != '') adminObj.provinceCode = province;
+
+  return adminObj;
+}
+
+function fillPartnersField(obj) {
+  if (obj.partners)
+    obj.partners.forEach(function(partner, i) {
+      let id;
+      if (i == 0) id = '';
+      else {
+        id = i;
+        addMoreGroup($('#addMorepartners'));
+      }
+      $('#partners' + id).val(partner.adminCode);
+      showFieldsPartner(id, false);
+      if (partner.email) $('#partnerscontactemail' + id).val(partner.email);
+      if (partner.name) $('#partnerscontactname' + id).val(partner.name);
+    });
+}
+
+function getmoreIndex(element) {
+  let nb = $(element)
+    .closest('li')
+    .attr('data-index');
+  return nb != 0 ? nb : '';
+}
+
+function selectPartners(select) {
+  let adminCode = $(select).val();
+  let id = getmoreIndex(select);
+  if (adminCode != '') {
+    $.getJSON(
+      'https://canada-ca.github.io/ore-ero/administrations.json',
+      function(result) {
+        let admin = getAdminObjectForPartner(result, adminCode);
+        showFieldsPartner(id, false);
+        $('#orgLevelPartner' + id).val(admin.level);
+        if (admin.values.provinceCode != undefined) {
+          $('#provinceSelectPartner' + id)
+            .prop('disabled', false)
+            .val(admin.values.provinceCode);
+        } else
+          $('#provinceSelectPartner' + id)
+            .prop('disabled', true)
+            .val('');
+        $('#enpartnersname' + id).val(admin.values.name.en);
+        $('#frpartnersname' + id).val(admin.values.name.fr);
+      }
+    );
+  } else {
+    hideFieldsPartner(id);
+  }
+}
+
+function hideFieldsPartner(id) {
+  resetFieldsPartner(id);
+  hideFieldPartner($('#orgLevelPartner' + id));
+  hideFieldPartner($('#provinceSelectPartner' + id));
+  hideFieldPartner($('#enpartnersname' + id));
+  hideFieldPartner($('#frpartnersname' + id));
+  $('#partnersNewAdminSeparator' + id).hide();
+  hideFieldPartner($('#partnerscontactname' + id));
+  hideFieldPartner($('#partnerscontactemail' + id));
+}
+
+function hideFieldPartner(element) {
+  element
+    .prop('disabled', false)
+    .parent('.form-group')
+    .hide();
+}
+
+function resetFieldsPartner(id) {
+  $('#orgLevelPartner' + id).val('');
+  $('#provinceSelectPartner' + id)
+    .prop('disabled', false)
+    .val('');
+  $('#enpartnersname' + id).val('');
+  $('#frpartnersname' + id).val('');
+  $('#partnerscontactname' + id).val('');
+  $('#partnerscontactemail' + id).val('');
+}
+
+function showFieldsPartner(id, full) {
+  hideFieldsPartner(id);
+  if (full) {
+    showFieldPartner($('#orgLevelPartner' + id));
+    showFieldPartner($('#provinceSelectPartner' + id));
+    if ($('#orgLevelPartner' + id).val() != 'municipal')
+      $('#provinceSelectPartner' + id)
+        .removeAttr('required')
+        .siblings('label')
+        .removeClass('required');
+    showFieldPartner($('#enpartnersname' + id));
+    showFieldPartner($('#frpartnersname' + id));
+    $('#partnersNewAdminSeparator' + id).show();
+  }
+
+  showFieldPartnerOptional(id);
+}
+
+function showFieldPartner(element) {
+  element
+    .attr('required', 'required')
+    .prop('disabled', false)
+    .parent('.form-group')
+    .show()
+    .children('label')
+    .addClass('required');
+}
+
+function showFieldPartnerOptional(id) {
+  $('#partnerscontactname' + id)
+    .parent('.form-group')
+    .show();
+
+  $('#partnerscontactemail' + id)
+    .parent('.form-group')
+    .show();
+}
+
+function addNewPartner(button) {
+  let id = getmoreIndex(button);
+
+  $('#partners' + id).val('');
+  resetFieldsPartner(id);
+  showFieldsPartner(id, true);
+  showRemoveNewPartnerBtn(id);
+}
+
+function removeNewPartner(button) {
+  let id = getmoreIndex(button);
+
+  $('#partners' + id).val('');
+  resetFieldsPartner(id);
+  hideFieldsPartner(id);
+  hideRemoveNewPartnerBtn(id);
+}
+
+function showRemoveNewPartnerBtn(index) {
+  $('newAdminButtonpartners' + index).hide();
+}
+
+function hideRemoveNewPartnerBtn(index) {
+  $('newAdminButtonpartners' + index).hide();
+}
+
+function getAdminObjectForPartner(obj, admin) {
+  let administrations = [
+    'federal',
+    'provincial',
+    'municipal',
+    'aboriginal',
+    'others'
+  ];
+
+  for (let i = 0, l1 = administrations.length; i < l1; i++)
+    for (let j = 0, l2 = obj[administrations[i]].length; j < l2; j++)
+      if (obj[administrations[i]][j].code == admin)
+        return {
+          level: administrations[i],
+          values: obj[administrations[i]][j]
+        };
+}
