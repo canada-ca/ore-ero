27c80587b5ad4288e58efd8e5feacf8cf13cd980
Simon <simon_moreau@hotmail.ca>
2020-01-31 12:41:41 -0500

    Issue #968 Pull licenses list

---
+++ assets/js/src/licences.js
@@ -0,0 +1,29 @@
+
+const licenseSelect = document.getElementById('licencesspdxID');
+$(document).ready(function() {
+    $(document).on('ready', function() {
+        getLicenses();
+    });
+});
+
+function getLicenses() {
+    $.getJSON('https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json', function(
+    result
+    ) {
+        addOptions(result);
+    });
+}
+
+function addOptions(list) {
+    for (let i = 0; i < list.licenses.length; i++) {
+        let newOption = document.createElement('option');
+        newOption.text = list.licenses[i].licenseId;
+        newOption.value = list.licenses[i].licenseId;
+        licenseSelect.appendChild(newOption);
+    }
+    let nonSPDX = document.createElement('option');
+    nonSPDX.text = 'Non-SPDX-and-Public-Domain';
+    nonSPDX.value = 'Non-SPDX-and-Public-Domain';
+    licenseSelect.appendChild(nonSPDX);
+}
+
