4c48370790e386d8eb96ba9ab0273a95678990c8
Simon <simon_moreau@hotmail.ca>
2020-01-31 13:19:34 -0500

    Prettified code

--- assets/js/src/licences.js
+++ assets/js/src/licences.js
@@ -1,25 +1,24 @@
-
 const licenseSelect = document.getElementById('licencesspdxID');
 $(document).ready(function() {
-    getLicenses();
+  getLicenses();
 });
 
 function getLicenses() {
-    $.getJSON('https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json', function(
-    result
-    ) {
-        addOptions(result);
-    });
+  $.getJSON(
+    'https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json',
+    function(result) {
+      addOptions(result);
+    }
+  );
 }
 
 function addOptions(list) {
-    for (let i = 0; i < list.licenses.length; i++) {
-        let newOption = document.createElement('option');
-        newOption.text = newOption.value = list.licenses[i].licenseId;
-        licenseSelect.appendChild(newOption);
-    }
-    let nonSPDX = document.createElement('option');
-    nonSPDX.text = nonSPDX.value = 'Non-SPDX-or-Public-Domain';
-    licenseSelect.appendChild(nonSPDX);
+  for (let i = 0; i < list.licenses.length; i++) {
+    let newOption = document.createElement('option');
+    newOption.text = newOption.value = list.licenses[i].licenseId;
+    licenseSelect.appendChild(newOption);
+  }
+  let nonSPDX = document.createElement('option');
+  nonSPDX.text = nonSPDX.value = 'Non-SPDX-or-Public-Domain';
+  licenseSelect.appendChild(nonSPDX);
 }
-
