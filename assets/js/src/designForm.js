2cf235e9d7b40ccdee1c849813ac05027effc401
Simon <Simon.moreau@montreal.ca>
2020-02-05 13:01:42 -0500

    changed design form variable
    Most of the variable still had software name on them, breaking the form for the open design

---
+++ assets/js/src/designForm.js
@@ -0,0 +1,453 @@
+/*
+  global
+  YamlWriter jsyaml
+  USERNAME REPO_NAME PRBOT_URL
+  getTagsEN getTagsFR resetTags addTags
+  submitInit submitConclusion
+  getAdminObject getAdminCode slugify
+  addMoreLicences resetMoreGroup fillLicenceField
+  getToday
+*/
+
+const softwareSelect = $('.page-designForm #nameselect');
+const adminSelect = $('.page-designForm #adminCode');
+
+$(document).ready(function() {
+  $('#prbotSubmitdesignForm').click(function() {
+    if (submitInit()) {
+      if ($('#ennewAdminName').val() != '') DesignsoftwareFormNewAdmin();
+      else submitFormSoftware();
+    }
+  });
+
+  softwareSelect.change(function() {
+    selectSoftware();
+    if (adminSelect.val() != '') selectAdmin();
+  });
+
+  adminSelect.change(function() {
+    selectAdmin();
+  });
+
+  $('#formReset').click(function() {
+    $('#validation').trigger('reset');
+    resetTags();
+  });
+});
+
+function getdesignObject() {
+  // Mandatory fields
+  let designObject = {
+    schemaVersion: '1.0',
+    description: {
+      whatItDoes: {
+        en: $('#endescriptionwhatItDoes').val(),
+        fr: $('#frdescriptionwhatItDoes').val()
+      }
+    },
+    category: $('#category :selected').val(),
+    homepageURL: {
+      en: $('#enhomepageURL').val(),
+      fr: $('#frhomepageURL').val()
+    },
+    licences: [],
+    name: {
+      en: $('#enname').val(),
+      fr: $('#frname').val()
+    },
+    tags: {
+      en: getTagsEN(),
+      fr: getTagsFR()
+    },
+    administrations: [
+      {
+        adminCode: getAdminCode(),
+        uses: [
+          {
+            contact: {
+              email: $('#contactemail').val()
+            },
+            date: {
+              started: $('#date').val(),
+              metadataLastUpdated: getToday()
+            }
+          }
+        ]
+      }
+    ]
+  };
+
+  // More-groups
+  addMoreLicences(designObject);
+
+  // Optional fields
+  if (
+    $('#endescriptionhowItWorks').val() ||
+    $('#frdescriptionhowItWorks').val()
+  ) {
+    designObject.description.howItWorks = {};
+  }
+  if ($('#endescriptionhowItWorks').val()) {
+    designObject.description.howItWorks.en = $(
+      '#endescriptionhowItWorks'
+    ).val();
+  }
+  if ($('#frdescriptionhowItWorks').val()) {
+    designObject.description.howItWorks.fr = $(
+      '#frdescriptionhowItWorks'
+    ).val();
+  }
+
+  if ($('#contactname').val()) {
+    designObject.administrations[0].uses[0].contact.name = $(
+      '#contactname'
+    ).val();
+  }
+
+  if ($('#enteam').val() || $('#frteam').val()) {
+    designObject.administrations[0].uses[0].team = {};
+    if ($('#enteam').val())
+      designObject.administrations[0].uses[0].team.en = $('#enteam').val();
+    if ($('#frteam').val())
+      designObject.administrations[0].uses[0].team.fr = $('#frteam').val();
+  }
+
+  return designObject;
+}
+
+function getSelectedOrgType() {
+  if ($('#adminCode').val() != '')
+    return $('#adminCode :selected')
+      .parent()
+      .attr('label')
+      .toLowerCase();
+  else return $('#orgLevel').val();
+}
+
+function DesignsoftwareFormNewAdmin() {
+  let submitButton = document.getElementById('prbotDesignsoftwareForm');
+  let resetButton = document.getElementById('formReset');
+  submitButton.disabled = true;
+  resetButton.disabled = true;
+
+  let designObject = getdesignObject();
+  let adminObject = getAdminObject();
+  let softwareName = $('#enname').val();
+  let adminName = slugify(
+    $('#ennewAdminName').val() + '-' + $('#provinceSelect').val()
+  );
+
+  let fileWriter = new YamlWriter(USERNAME, REPO_NAME);
+  let softwareFile = `_data/software/${slugify(softwareName)}.yml`;
+  let adminFile = `_data/administrations/${getSelectedOrgType()}.yml`;
+
+  fileWriter
+    .mergeAdminFile(adminFile, adminObject, '', 'code')
+    .then(adminResult => {
+      fileWriter
+        .merge(softwareFile, designObject, 'administrations', 'adminCode')
+        .then(softwareResult => {
+          return fetch(
+            PRBOT_URL,
+            getConfigUpdateSoftwareNewAdmin(
+              softwareName,
+              adminName,
+              softwareFile,
+              adminFile,
+              softwareResult,
+              adminResult
+            )
+          );
+        })
+        .catch(err => {
+          if (err.status == 404) {
+            return fetch(
+              PRBOT_URL,
+              getConfigNewSoftwareNewAdmin(
+                softwareName,
+                adminName,
+                softwareFile,
+                adminFile,
+                designObject,
+                adminResult
+              )
+            );
+          } else throw err;
+        })
+        .then(response => {
+          let url =
+            $('html').attr('lang') == 'en'
+              ? './open-source-softwares.html'
+              : './logiciels-libres.html';
+          submitConclusion(response, submitButton, resetButton, url);
+        });
+    });
+}
+
+function getConfigUpdateSoftwareNewAdmin(
+  softwareName,
+  adminName,
+  softwareFile,
+  adminFile,
+  softwareResult,
+  adminObject
+) {
+  return {
+    body: JSON.stringify({
+      user: USERNAME,
+      repo: REPO_NAME,
+      title:
+        'Updated software file for ' +
+        softwareName +
+        ' and created ' +
+        adminName +
+        ' in administration file',
+      description: 'Authored by: ' + $('#submitteremail').val() + '\n',
+      commit: 'Committed by ' + $('#submitteremail').val(),
+      author: {
+        name: $('#submitterusername').val(),
+        email: $('#submitteremail').val()
+      },
+      files: [
+        {
+          path: softwareFile,
+          content: '---\n' + jsyaml.dump(softwareResult)
+        },
+        {
+          path: adminFile,
+          content: '---\n' + jsyaml.dump(adminObject)
+        }
+      ]
+    }),
+    method: 'POST'
+  };
+}
+
+function getConfigNewSoftwareNewAdmin(
+  softwareName,
+  adminName,
+  softwareFile,
+  adminFile,
+  designObject,
+  adminObject
+) {
+  return {
+    body: JSON.stringify({
+      user: USERNAME,
+      repo: REPO_NAME,
+      title:
+        'Creaded software file for ' +
+        softwareName +
+        ' and created ' +
+        adminName +
+        ' in administration file',
+      description: 'Authored by: ' + $('#submitteremail').val() + '\n',
+      commit: 'Committed by ' + $('#submitteremail').val(),
+      author: {
+        name: $('#submitterusername').val(),
+        email: $('#submitteremail').val()
+      },
+      files: [
+        {
+          path: softwareFile,
+          content: '---\n' + jsyaml.dump(designObject)
+        },
+        {
+          path: adminFile,
+          content: '---\n' + jsyaml.dump(adminObject)
+        }
+      ]
+    }),
+    method: 'POST'
+  };
+}
+
+function submitFormSoftware() {
+  let submitButton = document.getElementById('prbotDesignsoftwareForm');
+  let resetButton = document.getElementById('formReset');
+  submitButton.disabled = true;
+  resetButton.disabled = true;
+
+  let designObject = getdesignObject();
+  let fileWriter = new YamlWriter(USERNAME, REPO_NAME);
+  let ProjectName = $('#enname').val();
+  let file = `_data/software/${slugify(ProjectName)}.yml`;
+
+  fileWriter
+    .merge(file, designObject, 'administrations', 'adminCode')
+    .then(result => {
+      return fetch(PRBOT_URL, getConfigUpdate(result, file, ProjectName));
+    })
+    .catch(err => {
+      if (err.status == 404) {
+        return fetch(PRBOT_URL, getConfigNew(designObject, file, ProjectName));
+      } else throw err;
+    })
+    .then(response => {
+      let url =
+        $('html').attr('lang') == 'en'
+          ? './open-source-softwares.html'
+          : './logiciels-libres.html';
+      submitConclusion(response, submitButton, resetButton, url);
+    });
+}
+
+function getConfigUpdate(result, file, ProjectName) {
+  return {
+    body: JSON.stringify({
+      user: USERNAME,
+      repo: REPO_NAME,
+      title: `Updated the ${ProjectName} software file`,
+      description:
+        'Authored by: ' +
+        $('#submitteremail').val() +
+        '\n' +
+        'Project: ***' +
+        $('#enname').val() +
+        '***\n' +
+        $('#endescriptionwhatItDoes').val() +
+        '\n',
+      commit: 'Committed by ' + $('#submitteremail').val(),
+      author: {
+        name: $('#submitterusername').val(),
+        email: $('#submitteremail').val()
+      },
+      files: [
+        {
+          path: file,
+          content: '---\n' + jsyaml.dump(result)
+        }
+      ]
+    }),
+    method: 'POST'
+  };
+}
+
+function getConfigNew(designObject, file, ProjectName) {
+  return {
+    body: JSON.stringify({
+      user: USERNAME,
+      repo: REPO_NAME,
+      title: 'Created the software file for ' + ProjectName,
+      description:
+        'Authored by: ' +
+        $('#submitteremail').val() +
+        '\n' +
+        'Project: ***' +
+        $('#enname').val() +
+        '***\n' +
+        $('#endescriptionwhatItDoes').val() +
+        '\n',
+      commit: 'Committed by ' + $('#submitteremail').val(),
+      author: {
+        name: $('#submitterusername').val(),
+        email: $('#submitteremail').val()
+      },
+      files: [
+        {
+          path: file,
+          content: '---\n' + jsyaml.dump(designObject)
+        }
+      ]
+    }),
+    method: 'POST'
+  };
+}
+
+function selectSoftware() {
+  let value = softwareSelect.val();
+  $.getJSON('http://canada-ca.github.io/ore-ero/design.json', function(result) {
+    if (result[value]) {
+      addValueToFieldsSoftware(result[value]);
+      $('#adminCode').focus();
+    } else if (value == '') {
+      resetFieldsSoftware();
+    } else {
+      alert('Error retrieving the data');
+    }
+  });
+}
+
+function addValueToFieldsSoftware(obj) {
+  resetFieldsSoftware();
+
+  $('#enname')
+    .val(obj.name.en)
+    .prop('disabled', true);
+  $('#frname')
+    .val(obj.name.fr)
+    .prop('disabled', true);
+  $('#endescriptionwhatItDoes').val(obj.description.whatItDoes.en);
+  $('#frdescriptionwhatItDoes').val(obj.description.whatItDoes.fr);
+
+  if (obj.description.howItWorks) {
+    if (obj.description.howItWorks.en)
+      $('#endescriptionhowItWorks').val(obj.description.howItWorks.en);
+    if (obj.description.howItWorks.fr)
+      $('#frdescriptionhowItWorks').val(obj.description.howItWorks.fr);
+  }
+
+  $('#category').val(obj.category);
+  $('#enhomepageURL').val(obj.homepageURL.en);
+  $('#frhomepageURL').val(obj.homepageURL.fr);
+  fillLicenceField(obj.licences);
+  addTags(obj);
+}
+
+function resetFieldsSoftware() {
+  $('#enname')
+    .val('')
+    .prop('disabled', false);
+  $('#frname')
+    .val('')
+    .prop('disabled', false);
+  $('#endescriptionwhatItDoes').val('');
+  $('#frdescriptionwhatItDoes').val('');
+  $('#endescriptionhowItWorks').val('');
+  $('#frdescriptionhowItWorks').val('');
+  $('#category').val('');
+  $('#enhomepageURL').val('');
+  $('#frhomepageURL').val('');
+  resetMoreGroup($('#addMorelicences'));
+  resetTags();
+}
+
+function selectAdmin() {
+  let software = softwareSelect.val();
+  let administration = adminSelect.val();
+  $.getJSON('https://canada-ca.github.io/ore-ero/software.json', function(
+    result
+  ) {
+    if (result[software]) {
+      for (let i = 0; i < result[software].administrations.length; i++) {
+        if (result[software].administrations[i].adminCode == administration) {
+          addValueToFieldsAdmin(result[software].administrations[i]);
+          break;
+        } else {
+          resetFieldsAdmin();
+        }
+      }
+    }
+  });
+}
+
+function addValueToFieldsAdmin(obj) {
+  resetFieldsAdmin();
+
+  $('#contactemail').val(obj.uses[0].contact.email);
+  if (obj.uses[0].contact.name) $('#contactname').val(obj.uses[0].contact.name);
+
+  $('#date').val(obj.uses[0].date.started);
+  if (obj.team) {
+    if (obj.team.en) $('#enteam').val(obj.team.en);
+    if (obj.team.fr) $('#frteam').val(obj.team.fr);
+  }
+}
+
+function resetFieldsAdmin() {
+  $('#contactemail').val('');
+  $('#contactname').val('');
+  $('#date').val('');
+  $('#enteam').val('');
+  $('#frteam').val('');
+}
