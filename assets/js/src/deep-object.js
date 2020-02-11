c7be04574534eaa2df480ed7c60c411105d380ff
Joseph <josephaime.djemo@gmail.com>
2020-02-11 13:53:14 -0500

    Fixing deep-object tests

--- assets/js/src/deep-object.js
+++ assets/js/src/deep-object.js
@@ -3,7 +3,7 @@
 // @author j-rewerts
 
 /* exported DeepObject */
-const DeepObject = {
+  window.DeepObject = {
   /**
    * Attempts to get the object at path.
    * @param {Object} obj The Object to try to find.
