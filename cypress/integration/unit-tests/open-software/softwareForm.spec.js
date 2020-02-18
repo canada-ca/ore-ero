bed13e31c36bb6a66a09cf7133f8c4f7d9fb0219
Joseph <josephaime.djemo@gmail.com>
2020-02-18 12:02:25 -0500

    Addition of test to selecting and filling software information

--- cypress/integration/unit-tests/open-software/softwareForm.spec.js
+++ cypress/integration/unit-tests/open-software/softwareForm.spec.js
@@ -2,16 +2,28 @@
 /* global cy, context, it */
 
 context('Open Source Software', () => {
-    
-    // before(() => {
-    //     cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
-    //     cy.get('#wb-cont').contains('Open Source Software Form');
-    // });
+  it('Loads the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
+    cy.get('#wb-cont').contains('Open Source Software Form');
+  });
 
-    it('Loads the English page', () => {
-        cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
-        cy.get('#wb-cont').contains('Open Source Software Form');
-    });
+  it('Should return dynamicall filled of existing data information on the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
+    cy.get('select')
+      .as('option')
+      .invoke('val', 'net-core')
+      .first()
+      .trigger('change')
+      .contains('.NET Core');
+  });
 
-
+  it('Should not dynamicall filled of unexisting data information on the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
+    cy.get('select')
+      .as('option')
+      .invoke('val', 'net-core')
+      .first()
+      .trigger('change')
+      .should('not.contain', 'sourcetree');
+  });
 });
