0cc203bc369b48396991df6bf584760b577009de
Simon <Simon.moreau@montreal.ca>
2020-02-19 13:27:10 -0500

    Started reset test and finished fill test

--- cypress/integration/unit-tests/open-design/design-form.spec.js
+++ cypress/integration/unit-tests/open-design/design-form.spec.js
@@ -18,7 +18,10 @@ context('Open Design Form', () => {
           cy.window().get('#frdescriptionwhatItDoes').should('not.have.value', '');
           cy.window().get('#enhomepageURL').should('not.have.value', '');
           cy.window().get('#frhomepageURL').should('not.have.value', '');
-          cy.window().get('#designType').children().first().should('not.be.selected');
+          cy.window().get('#addMoredesignType').find('ul > li').each(($li, id) => {
+            let i = id == 0 ? '' : id;
+            cy.window().get('#designType' + i).children().first().should('not.be.selected');
+          });
           cy.window().get('#addMorelicences').find('ul > li').each(($li, id) => {
             let i = id == 0 ? '' : id;
             cy.window().get('#enlicencesURL' + i).should('not.have.value', '');
@@ -26,6 +29,30 @@ context('Open Design Form', () => {
             cy.window().get('#licencesspdxID' + i).children().first().should('not.be.selected');
           });
         });
+    });
+
+    it('Should reset all fields when reset is activated', () => {
+        cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
+        cy.window().get('#formReset').click().then(() => {
+            cy.window().get('#adminCode').children().first().should('be.selected');
+            cy.window().get('#nameselect').children().first().should('be.selected');
+            cy.window().get('#enname').should('have.value', '');
+            cy.window().get('#frname').should('have.value', '');
+            cy.window().get('#endescriptionwhatItDoes').should('have.value', '');
+            cy.window().get('#frdescriptionwhatItDoes').should('have.value', '');
+            cy.window().get('#enhomepageURL').should('have.value', '');
+            cy.window().get('#frhomepageURL').should('have.value', '');
+            cy.window().get('#addMoredesignType').find('ul > li').each(($li, id) => {
+              let i = id == 0 ? '' : id;
+              cy.window().get('#designType' + i).children().first().should('be.selected');
+            });
+            cy.window().get('#addMorelicences').find('ul > li').each(($li, id) => {
+              let i = id == 0 ? '' : id;
+              cy.window().get('#enlicencesURL' + i).should('have.value', '');
+              cy.window().get('#frlicencesURL' + i).should('have.value', '');
+              cy.window().get('#licencesspdxID' + i).children().first().should('be.selected');
+            });
+          });
     });
 
     it('Loads the script on french page', () => {
@@ -43,7 +70,10 @@ context('Open Design Form', () => {
           cy.window().get('#frdescriptionwhatItDoes').should('not.have.value', '');
           cy.window().get('#enhomepageURL').should('not.have.value', '');
           cy.window().get('#frhomepageURL').should('not.have.value', '');
-          cy.window().get('#designType').children().first().should('not.be.selected');
+          cy.window().get('#addMoredesignType').find('ul > li').each(($li, id) => {
+            let i = id == 0 ? '' : id;
+            cy.window().get('#designType' + i).children().first().should('not.be.selected');
+          });
           cy.window().get('#addMorelicences').find('ul > li').each(($li, id) => {
             let i = id == 0 ? '' : id;
             cy.window().get('#enlicencesURL' + i).should('not.have.value', '');
