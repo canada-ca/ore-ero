f6a74646ff5fbf0569c321337b4e4dfa8266118a
Simon <Simon.moreau@montreal.ca>
2020-02-19 15:09:49 -0500

    Working on design table test

--- cypress/integration/unit-tests/open-design/design.spec.js
+++ cypress/integration/unit-tests/open-design/design.spec.js
@@ -80,3 +80,63 @@ context('Open Design', () => {
         cy.get('#wb-cont').contains('Open Design Form');
     });
 });
+
+
+context('Common parts', () => {
+    const selectTags = [
+      '#dt_govLevel',
+      '#dt_licence',
+      '#dt_type',
+      '#dt_status'
+    ];
+    // eslint-disable-next-line no-undef
+    beforeEach(() => {
+      cy.visit('http://localhost:4000/ore-ero/en/open-design.html');
+    });
+
+    it('should reset inputs', function() {
+      //select first element of the select
+      //should test if it's null
+      selectTags.forEach(selectTag => {
+        cy.get(`${selectTag} > option`)
+          .eq(1)
+          .then(element => {
+            cy.get(selectTag).select(element.val());
+          });
+      });
+
+      cy.get('.wb-tables-filter > .row > :nth-child(2) > .btn').click();
+
+      selectTags.forEach(selectTag => {
+        cy.get(`${selectTag} > option`)
+          .eq(0)
+          .should('be.selected');
+      });
+    });
+
+    it('should filter the first element with the right values', () => {
+      cy.get('.sorting_1')
+        .first()
+        .then(projectName => {
+          cy.get('tbody > :nth-child(1) > :nth-child(4)').then(type => {
+            cy.get(`#dt_type`).select(type.text().trim());
+          });
+          cy.get('tbody > :nth-child(1) > :nth-child(3)').then(licence => {
+            cy.get(`#dt_licence`).select(licence.text().trim());
+          });
+          cy.get('.wb-tables-filter > .row > :nth-child(1) > .btn').click();
+          cy.get(':nth-child(1) > .sorting_1').contains(projectName.text());
+        });
+    });
+
+    it('should open the correct modal', () => {
+      cy.get(':nth-child(1) > .sorting_1').click();
+      cy.get(':nth-child(1) > .sorting_1').then(name => {
+        cy.get('.wb-overlay .open').contains(name.text());
+      });
+
+      // click on the close button
+      cy.get('.wb-overlay-close button').click();
+      cy.get('.wb-overlay').should('not.be.visible');
+    });
+  });
