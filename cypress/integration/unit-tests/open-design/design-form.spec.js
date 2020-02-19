7ed391a50594bc2bc4e7f986d91ed3250eb5e7f5
Simon <Simon.moreau@montreal.ca>
2020-02-19 12:55:17 -0500

    Design select tested

--- cypress/integration/unit-tests/open-design/design-form.spec.js
+++ cypress/integration/unit-tests/open-design/design-form.spec.js
@@ -1,6 +1,55 @@
 /// <reference types="Cypress" />
 /* global cy, context, it */
 
-context('Open Design', () => {
-    
+context('Open Design Form', () => {
+
+    it('Loads the script', () => {
+        cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
+        cy.window().should('have.property', 'designType');
+        });
+
+    it('Values should be autofilled when design name is selected', () => {
+        cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
+        cy.window().get('#nameselect').children().eq(1).invoke('text').then((name) => {
+          cy.window().get('#nameselect').select(name);
+          cy.window().get('#enname').should('have.value', name);
+          cy.window().get('#frname').should('not.have.value', '');
+          cy.window().get('#endescriptionwhatItDoes').should('not.have.value', '');
+          cy.window().get('#frdescriptionwhatItDoes').should('not.have.value', '');
+          cy.window().get('#enhomepageURL').should('not.have.value', '');
+          cy.window().get('#frhomepageURL').should('not.have.value', '');
+          cy.window().get('#designType').children().first().should('not.be.selected');
+          cy.window().get('#addMorelicences').find('ul > li').each(($li, id) => {
+            let i = id == 0 ? '' : id;
+            cy.window().get('#enlicencesURL' + i).should('not.have.value', '');
+            cy.window().get('#frlicencesURL' + i).should('not.have.value', '');
+            cy.window().get('#licencesspdxID' + i).children().first().should('not.be.selected');
+          });
+        });
+    });
+
+    it('Loads the script on french page', () => {
+        cy.visit('http://localhost:4000/ore-ero/fr/design-libre-formulaire.html');
+        cy.window().should('have.property', 'designType');
+        });
+
+    it('Values should be autofilled when design name is selected on french page', () => {
+        cy.visit('http://localhost:4000/ore-ero/fr/design-libre-formulaire.html');
+        cy.window().get('#nameselect').children().eq(1).invoke('text').then((name) => {
+          cy.window().get('#nameselect').select(name);
+          cy.window().get('#enname').should('not.have.value', '');
+          cy.window().get('#frname').should('have.value', name);
+          cy.window().get('#endescriptionwhatItDoes').should('not.have.value', '');
+          cy.window().get('#frdescriptionwhatItDoes').should('not.have.value', '');
+          cy.window().get('#enhomepageURL').should('not.have.value', '');
+          cy.window().get('#frhomepageURL').should('not.have.value', '');
+          cy.window().get('#designType').children().first().should('not.be.selected');
+          cy.window().get('#addMorelicences').find('ul > li').each(($li, id) => {
+            let i = id == 0 ? '' : id;
+            cy.window().get('#enlicencesURL' + i).should('not.have.value', '');
+            cy.window().get('#frlicencesURL' + i).should('not.have.value', '');
+            cy.window().get('#licencesspdxID' + i).children().first().should('not.be.selected');
+          });
+        });
+    });
 });
--- cypress/integration/unit-tests/open-design/designType.spec.js
+++ cypress/integration/unit-tests/open-design/designType.spec.js
@@ -2,21 +2,7 @@
 /* global cy, context, it, expect */
 
 context('Open Design Form', () => {
-    
-
-it('Loads the script', () => {
-  cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
-  cy.window().should('have.property', 'designType');
-  });
-
-// it ('should reset newType input fields when resetType method is called', () => {
-//     cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
-//     cy.window().get('#newTypeButton').click();
-//     cy.window().get('#ennewType').type('Test');
-//     cy.window().invoke('resetType', '');
-//     cy.window().get('#ennewType').should('be.equal', ''); 
-//   });
-    
+   
 it ('New type button should make new type field appear if hidden and both new type button and remove new type button should hide it if shown', () => {
   cy.visit('http://localhost:4000/ore-ero/en/open-design-form.html');
   cy.window().get('#newTypeButton').click();
@@ -36,5 +22,25 @@ it ('newType button and designType select should return the other to default sta
   cy.window().get('#newTypeButton').click();
   cy.window().get('#designType').children().first().should('be.selected');  
   });
+
+  it ('New type button should make new type field appear if hidden and both new type button and remove new type button should hide it if shown on french page', () => {
+    cy.visit('http://localhost:4000/ore-ero/fr/design-libre-formulaire.html');
+    cy.window().get('#newTypeButton').click();
+    cy.window().get('#newType').should('be.visible');
+    cy.window().get('#newTypeButton').click();
+    cy.window().get('#newType').should('be.hidden');
+    cy.window().get('#newTypeButton').click();
+    cy.window().get('#cancelnewTypeButton').click();
+    cy.window().get('#newType').should('be.hidden');
+    });
+  
+  it ('newType button and designType select should return the other to default state when clicked/changed on french page', () => {
+    cy.visit('http://localhost:4000/ore-ero/fr/design-libre-formulaire.html');
+    cy.window().get('#newTypeButton').click();
+    cy.window().get('#designType').select('Application');
+    cy.window().get('#newType').should('be.hidden');  
+    cy.window().get('#newTypeButton').click();
+    cy.window().get('#designType').children().first().should('be.selected');  
+    });
 
 });
