3b169fdbaecadf45dc9b665577201dd5be587453
Simon <Simon.moreau@montreal.ca>
2020-02-21 10:03:51 -0500

    fix selectAdmin call

--- assets/js/src/designForm.js
+++ assets/js/src/designForm.js
@@ -438,7 +438,7 @@ function resetFieldsDesign() {
 function selectAdmin() {
   let design = designSelect.val();
   let administration = adminSelect.val();
-  $.getJSON('http://localhost:4000/ore-ero/design.json', function(result) {
+  $.getJSON('https://canada-ca.github.io/ore-ero/design.json', function(result) {
     if (result[design]) {
       for (let i = 0; i < result[design].administrations.length; i++) {
         if (result[design].administrations[i].adminCode == administration) {
