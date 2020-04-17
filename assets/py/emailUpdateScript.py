882d09203670e7f937bbf40a0263a423489cb82c
Simon <Simon.moreau@montreal.ca>
2020-04-17 15:09:04 -0400

    Updated templates and the URLs where we get the data

--- assets/py/emailUpdateScript.py
+++ assets/py/emailUpdateScript.py
@@ -33,21 +33,20 @@ def sendEmails(emailData):
         ##Replace with data[0] to actually send the mails to the right place
         email['to'] = "Simon_moreau@hotmail.ca"
         email['subject'] = Templates.getSubject() 
-        plainVersion = MIMEText(Templates.plainFormat(data[1], data[2], data[3]), 'plain')
+        plainVersion = MIMEText(Templates.plainFormat(data[1], data[2], data[3], data[4]), 'plain')
         plainVersion.add_header("Content-Disposition",
         "attachment; filename= Plain Text Version.txt")
-        htmlVersion = MIMEText(Templates.htmlFormat(data[1], data[2], data[3]), 'html')
+        htmlVersion = MIMEText(Templates.htmlFormat(data[1], data[2], data[3], data[4]), 'html')
         email.attach(htmlVersion)
         email.attach(plainVersion)
         server.send_message(email)
         del email
         ##limit how many emails are sent during testing
-        break
     server.quit()
         
 
 def checkCodeEmails():
-    codeDb = urllib.request.urlopen("https://canada-ca.github.io/ore-ero/code.json")
+    codeDb = urllib.request.urlopen("https://code.open.canada.ca/code.json")
     data = json.loads(codeDb.read())
     codeData = []
     if data is not None:
@@ -55,14 +54,16 @@ def checkCodeEmails():
             for admin in level.values():
                 for release in admin["releases"]:
                     if (datetime.strptime(release["date"]["metadataLastUpdated"], '%Y-%m-%d').date()
-                    + timedelta(days=maxDaysNoUpdate) < date.today()): 
+                    + timedelta(days=maxDaysNoUpdate) < date.today()):
+                        if "noreply" not in release["contact"]["email"]:
                             codeData.append((release["contact"]["email"], release["name"]["en"],
-                            release["name"]["fr"], release["date"]["metadataLastUpdated"]))
+                            release["name"]["fr"], release["date"]["metadataLastUpdated"],
+                            "code"))
     return codeData
         
 
 def checkDesignEmails():
-    designDb = urllib.request.urlopen("https://canada-ca.github.io/ore-ero/design.json")
+    designDb = urllib.request.urlopen("https://code.open.canada.ca/design.json")
     data = json.loads(designDb.read())
     designData = []
     if data is not None:
@@ -70,13 +71,15 @@ def checkDesignEmails():
             for administration in project["administrations"]:
                 for use in administration["uses"]: 
                     if (datetime.strptime(use["date"]["metadataLastUpdated"], '%Y-%m-%d').date()
-                    + timedelta(days=maxDaysNoUpdate) < date.today()): 
+                    + timedelta(days=maxDaysNoUpdate) < date.today()):
+                        if "noreply" not in use["contact"]["email"]: 
                             designData.append((use["contact"]["email"], project["name"]["en"],
-                            project["name"]["fr"], use["date"]["metadataLastUpdated"]))
+                            project["name"]["fr"], use["date"]["metadataLastUpdated"],
+                            "design"))
     return designData
 
 def checkSoftwareEmails():
-    softwareDb = urllib.request.urlopen("https://canada-ca.github.io/ore-ero/software.json")
+    softwareDb = urllib.request.urlopen("https://code.open.canada.ca/software.json")
     data = json.loads(softwareDb.read())
     softwareData = []
     if data is not None:
@@ -84,26 +87,30 @@ def checkSoftwareEmails():
             for administration in project["administrations"]:
                 for use in administration["uses"]: 
                     if (datetime.strptime(use["date"]["metadataLastUpdated"], '%Y-%m-%d').date()
-                    + timedelta(days=maxDaysNoUpdate) < date.today()): 
+                    + timedelta(days=maxDaysNoUpdate) < date.today()):
+                        if "noreply" not in use["contact"]["email"]: 
                             softwareData.append((use["contact"]["email"], project["name"]["en"],
-                            project["name"]["fr"], use["date"]["metadataLastUpdated"]))
+                            project["name"]["fr"], use["date"]["metadataLastUpdated"],
+                            "software"))
     return softwareData
 
 def checkStandardEmails():
-    standardDb = urllib.request.urlopen("https://canada-ca.github.io/ore-ero/standard.json")
+    standardDb = urllib.request.urlopen("https://code.open.canada.ca/standard.json")
     data = json.loads(standardDb.read())
     standardData = []
     if data is not None:
         for project in data.values(): 
             for administration in project["administrations"]:
                 if (datetime.strptime(administration["date"]["metadataLastUpdated"], '%Y-%m-%d').date()
-                + timedelta(days=maxDaysNoUpdate) < date.today()): 
-                        standardData.append((administration["contact"]["email"], project["standardAcronym"],
-                        project["standardAcronym"], administration["date"]["metadataLastUpdated"]))
+                + timedelta(days=maxDaysNoUpdate) < date.today()):
+                        if "noreply" not in administration["contact"]["email"]: 
+                            standardData.append((administration["contact"]["email"], project["standardAcronym"],
+                            project["standardAcronym"], administration["date"]["metadataLastUpdated"],
+                            "standard"))
     return standardData
 
 def checkPartnershipEmails():
-    partnershipDb = urllib.request.urlopen("https://canada-ca.github.io/ore-ero/partnership.json")
+    partnershipDb = urllib.request.urlopen("https://code.open.canada.ca/partnership.json")
     data = json.loads(partnershipDb.read())
     partnershipData = []
     if data is not None:
@@ -112,8 +119,10 @@ def checkPartnershipEmails():
                 for project in admin["projects"]:
                     if (datetime.strptime(project["date"]["metadataLastUpdated"], '%Y-%m-%d').date()
                     + timedelta(days=maxDaysNoUpdate) < date.today()): 
+                        if "noreply" not in project["contact"]["email"]:
                             partnershipData.append((project["contact"]["email"], project["name"]["en"],
-                            project["name"]["fr"], project["date"]["metadataLastUpdated"]))
+                            project["name"]["fr"], project["date"]["metadataLastUpdated"],
+                            "partnership"))
     return partnershipData
 
 def checkOutdatedEmails():
--- assets/py/messageTemplates.py
+++ assets/py/messageTemplates.py
@@ -1,31 +1,93 @@
 class Templates:
+    frTypes = {"code": "code",
+        "design": "design",
+        "software": "logiciel",
+        "standard": "norme",
+        "partnership": "partenariat"}
+
+    selector = {"code": {"en": "open-source-code-form", "fr": "code-source-ouvert-formulaire"},
+        "design": {"en": "open-design-form", "fr": "design-libre-formulaire"},
+        "software": {"en": "open-source-software-form", "fr": "logiciel-libre-formulaire"},
+        "standard": {"en": "open-standard-form", "fr": "norme-ouverte-formulaire"},
+        "partnership": {"en": "partnership-form", "fr": "partenariat-formulaire"}}
+
     plain = """
     English Message:
-    Test message about {EN_NAME}, last updated {LAST_UPDATED}
+
+    Automated message about {EN_NAME} on the ORE platform, last updated {LAST_UPDATED}
+    You are receiving this message because our information concerning {EN_NAME} has not been updated
+    in the last 6 months and your email address is currently listed in the contact information for 
+    this {EN_TYPE}.
+    If you are no longer the contact for {EN_NAME},
+    you can use this form "https://code.open.canada.ca/en/{EN_FORM}.html"
+    to update our platform.
+
     "https://code.open.canada.ca/en/index.html"
 
     Message en français:
-    Message test concernant {FR_NAME}, mise à jour la plus récente {LAST_UPDATED}
+    Message automatisé concernant {FR_NAME} sur la plateforme Échange de Ressources Ouvert,
+     mise à jour la plus récente {LAST_UPDATED}
+    Vous recevez ce message parce que notre information concernant {FR_NAME} n'a pas été mis a jour
+    dans les 6 derniers mois et votre adresse email est inscrite comme adresse de contact pour 
+    ce {FR_TYPE}.
+    Si vous n'êtes plus la personne à contacter pour {FR_NAME},
+    vous pouvez utiliser ce formulaire "https://code.open.canada.ca/en/{FR_FORM}.html"
+    pour mettre a jour notre plateforme.
+
     "https://code.ouvert.canada.ca/fr/index.html"
     """
     html = """
-    <h4>English Message:</h4>
-    <p>Test message about {EN_NAME}, last updated {LAST_UPDATED} </p>
-    <p><a href="https://code.open.canada.ca/en/index.html">Link to site</a></p>
 
+    <h4>English Message:</h4>
+    <span>Automated message about {EN_NAME} on the Open Ressource Exchange platform
+    , last updated {LAST_UPDATED}</span>
+    <div>
+    <p>
+    You are receiving this message because our information concerning {EN_NAME} has not been updated
+    in the last 6 months and your email address is currently listed as the contact address for 
+    this {EN_TYPE}.
+    </p> 
+    <p>
+    If you are no longer the contact for {EN_NAME},
+    you can use this <a href="https://code.open.canada.ca/en/{EN_FORM}.html">form</a>
+    to update our platform.
+    </p>
+    </div>
+    <p><a href="https://code.open.canada.ca/en/index.html">Link to site</a></p>
+   
     <h4>Message en français:</h4>
-    <p>Message test concernant {FR_NAME}, mise à jour la plus récente {LAST_UPDATED} </p>
+    <span>Message automatisé concernant {FR_NAME} sur la plateforme Échange de Ressources Ouvert,
+     mise à jour la plus récente {LAST_UPDATED}</span>
+    <div>
+    <p>
+    Vous recevez ce message parce que notre information concernant {FR_NAME} n'a pas été mis a jour
+    dans les 6 derniers mois et votre adresse email est inscrite comme adresse de contact pour 
+    ce {FR_TYPE}.
+    </p> 
+    <p>
+    Si vous n'êtes plus la personne à contacter pour {FR_NAME},
+    vous pouvez utiliser ce <a href="https://code.open.canada.ca/en/{FR_FORM}.html">formulaire</a>
+    pour mettre a jour notre plateforme.
+    </p>
+    </div>
     <p><a href="https://code.ouvert.canada.ca/fr/index.html">Lien vers le site</a></p>
     """
     enSubject = "Keeping the Open Ressource Exchange platform up to date"
 
     frSubject = "Maintenir la plateforme Échange de Ressources Ouvert à Jour"
 
-    def plainFormat(enName, frName, lastUpdated):
-        return Templates.plain.format(EN_NAME=enName, LAST_UPDATED=lastUpdated, FR_NAME=frName)
-
-    def htmlFormat(enName, frName, lastUpdated):
-        return Templates.html.format(EN_NAME=enName, LAST_UPDATED=lastUpdated, FR_NAME=frName)
-
+    @staticmethod
+    def plainFormat(enName, frName, lastUpdated, category):
+        return Templates.plain.format(EN_NAME=enName, FR_NAME=frName, LAST_UPDATED=lastUpdated,
+        EN_TYPE=category, FR_TYPE=Templates.frTypes[category],
+        EN_FORM=Templates.selector[category]["en"], FR_FORM=Templates.selector[category]["fr"])
+    
+    @staticmethod
+    def htmlFormat(enName, frName, lastUpdated, category):
+        return Templates.html.format(EN_NAME=enName, FR_NAME=frName, LAST_UPDATED=lastUpdated,
+        EN_TYPE=category, FR_TYPE=Templates.frTypes[category],
+        EN_FORM=Templates.selector[category]["en"], FR_FORM=Templates.selector[category]["fr"])
+    
+    @staticmethod
     def getSubject():
         return Templates.enSubject + " // " + Templates.frSubject
