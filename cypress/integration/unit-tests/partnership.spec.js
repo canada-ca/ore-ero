e0f255ccd9cf85f25cc023ec50ad4bb70c31caad
Nick Schonning <nschonni@gmail.com>
2021-10-30 15:31:08 -0400

    chore: Update and expand ESLint

--- cypress/integration/unit-tests/partnership.spec.js
+++ cypress/integration/unit-tests/partnership.spec.js
@@ -2,35 +2,27 @@
 /* global cy, context, it */
 
 context('Partnerships', () => {
-    it('Loads the English page', () => {
-        cy.visit('http://localhost:4000/ore-ero/en/partnerships.html');
-        cy.get('#wb-cont').contains('Partnerships');
-    });
+  it('Loads the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/en/partnerships.html');
+    cy.get('#wb-cont').contains('Partnerships');
+  });
 
-    it('Dynamically filters on the English page', () => {
-        cy.visit('http://localhost:4000/ore-ero/en/partnerships.html');
-        cy.get('#dataset-filter_filter')
-            .find('input')
-            .type('docker');
+  it('Dynamically filters on the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/en/partnerships.html');
+    cy.get('#dataset-filter_filter').find('input').type('docker');
 
-        cy.get('#dataset-filter')
-            .find('tbody>tr')
-            .should('not.contain', 'docker');
-    });
+    cy.get('#dataset-filter').find('tbody>tr').should('not.contain', 'docker');
+  });
 
-    it('Loads the French page', () => {
-        cy.visit('http://localhost:4000/ore-ero/fr/partenariats.html');
-        cy.get('#wb-cont').contains('Partenariats');
-    });
+  it('Loads the French page', () => {
+    cy.visit('http://localhost:4000/ore-ero/fr/partenariats.html');
+    cy.get('#wb-cont').contains('Partenariats');
+  });
 
-    it('Dynamically filters on the English page', () => {
-        cy.visit('http://localhost:4000/ore-ero/fr/partenariats.html');
-        cy.get('#dataset-filter_filter')
-            .find('input')
-            .type('docker');
+  it('Dynamically filters on the English page', () => {
+    cy.visit('http://localhost:4000/ore-ero/fr/partenariats.html');
+    cy.get('#dataset-filter_filter').find('input').type('docker');
 
-        cy.get('#dataset-filter')
-            .find('tbody>tr')
-            .should('not.contain', 'docker');
-    });
+    cy.get('#dataset-filter').find('tbody>tr').should('not.contain', 'docker');
+  });
 });
--- cypress/integration/unit-tests/software-en-date.spec.js
+++ cypress/integration/unit-tests/software-en-date.spec.js
@@ -2,35 +2,35 @@
 /* global cy, context, it, before, expect */
 
 context('date_func.js', () => {
-    const now = new Date();
-    const output = (now.getFullYear() +
-        '-' +
-        ((now.getMonth() + 1) < 10 ? '0' : '') +
-        (now.getMonth() + 1) +
-        '-' +
-        ((now.getDate()) < 10 ? '0' : '') +
-        (now.getDate())
-    );
+  const now = new Date();
+  const output =
+    now.getFullYear() +
+    '-' +
+    (now.getMonth() + 1 < 10 ? '0' : '') +
+    (now.getMonth() + 1) +
+    '-' +
+    (now.getDate() < 10 ? '0' : '') +
+    now.getDate();
 
-    before(() => {
-        cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
-    });
+  before(() => {
+    cy.visit('http://localhost:4000/ore-ero/en/open-source-software-form.html');
+  });
 
-    it('Loads the script', () => {
-        cy.window().should('have.property', 'Date');
-    });
+  it('Loads the script', () => {
+    cy.window().should('have.property', 'Date');
+  });
 
-    it('It should call and return Date.now()', () => {
-        cy.window().then(win => {
-            let value = win.date_func.getToday();
-            expect(value).to.equal(output);
-        });
+  it('It should call and return Date.now()', () => {
+    cy.window().then((win) => {
+      let value = win.date_func.getToday();
+      expect(value).to.equal(output);
     });
+  });
 
-    it('It should not equal to day time', () => {
-        cy.window().then(win => {
-            const outputDate = new Date(Date.now());
-            expect(win.date_func.getToday()).not.equal(outputDate);
-        });
+  it('It should not equal to day time', () => {
+    cy.window().then((win) => {
+      const outputDate = new Date(Date.now());
+      expect(win.date_func.getToday()).not.equal(outputDate);
     });
+  });
 });
